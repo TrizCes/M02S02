ccbc99951fd3567b673ad673286f275ed9e40927
Beatriz Ceschini <biaceschini@gmail.com>
2023-04-12 20:57:05 -0300

    Código que verifica se o numero é par ou impar

---
+++ parOuImpar/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    int num;
+
+    Console.WriteLine("Olá! Vamos analisar o seu numero!");
+    Console.Write("Digite o numero:");
+    num = int.Parse(Console.ReadLine());
+
+    if (num % 2 == 0)
+    {
+      Console.WriteLine("O numero digitado é par!");
+    }
+    else
+    {
+      Console.WriteLine("O numero digitado é impar!");
+    }
+  }
+}
