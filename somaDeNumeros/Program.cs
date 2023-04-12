a92a3be77ec04d5e64acd0e2990cf42aea57c5cc
Beatriz Ceschini <biaceschini@gmail.com>
2023-04-12 19:43:42 -0300

    Criação da calculadora de soma de números digitados pelo usuário

---
+++ somaDeNumeros/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MyProject;
+class Program
+{
+  static void Main(string[] args)
+  {
+    int num1, num2, result;
+
+    Console.WriteLine("Olá! Vamos somar numeros!");
+    Console.Write("Digite o primeiro numero:");
+    num1 = int.Parse(Console.ReadLine());
+
+    Console.Write("Digite o segundo numero:");
+    num2 = int.Parse(Console.ReadLine());
+
+    result = num1 + num2;
+
+    Console.WriteLine("Resolução:");
+    Console.Write(num1 + " + " + num2 + " = " + result);
+  }
+}
