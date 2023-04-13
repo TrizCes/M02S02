98b0f1ed5e94b306b44856000d74dd4d808a9b63
Beatriz Ceschini <biaceschini@gmail.com>
2023-04-12 21:33:37 -0300

    Informações Veiculo
    Um programa que lê a Marca, Modelo e km rodados e mostre as informações do carro e se a KM for acima 10 mil, apresentar a mensagem que precisa ser feito uma revisão.

---
+++ informacoesVeiculos/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    string marca;
+    string modelo;
+    int kmRodados;
+
+    Console.WriteLine("Olá! Bem vindo(a) a central de informações de veículos!");
+    Console.Write("Digite a marca do seu veículo:");
+    marca = Console.ReadLine();
+    Console.Write("Digite o modelo do seu veículo:");
+    modelo = Console.ReadLine();
+    Console.Write("Informe quantos Km o veículo rodou:");
+    kmRodados = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Veículo {modelo}, da marca {marca}, com a quilometragem atual de {kmRodados}km. ");
+
+    if (kmRodados > 10000)
+    {
+      Console.WriteLine("Sua quilometragem ultrapassou o limite, é necessário realizar a revisão do veículo.");
+    }
+  }
+
+}
