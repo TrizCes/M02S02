5641703605fe647f1cdde9075a3d6ef24481924a
Beatriz Ceschini <biaceschini@gmail.com>
2023-04-12 21:15:23 -0300

    Ler o nome e a idade do paciente e mostrar idade e classificação etaria.
    Faça um programa que leia o nome e a idade do paciente e como resultado mostre o nome do paciente. E a mensagem se é menor ou maior de idade e se é idoso.

---
+++ nomeEIdade/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    string nome;
+    int idade = 0;
+
+    Console.WriteLine("Bem vinda(o) ao hospital C#");
+    Console.Write("Me infome o nome do paciente:");
+    nome = Console.ReadLine();
+    Console.Write("Agora me informe a idade do paciente:");
+    idade = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("O paciente " + nome + " tem " + idade + " anos.");
+
+    if (idade < 18)
+    {
+      Console.WriteLine(nome + " é menor de idade. Precisa de acompanhante.");
+    }
+    else if (idade >= 18 && idade < 65)
+    {
+      Console.WriteLine(nome + " é maior de idade.");
+    }
+    else
+    {
+      Console.WriteLine(nome + " é idoso. Precisa de acompanhante.");
+    }
+  }
+}
