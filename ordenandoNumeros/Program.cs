ab8a00cab18af12d87cb379c997ae9ac5c5bdc17
Beatriz Ceschini <biaceschini@gmail.com>
2023-04-13 20:50:14 -0300

    Ordenando números
    Um programa que lê 4 números e faz a ordenação entre eles.

---
+++ ordenandoNumeros/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OdenandoNumeros
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] numeros = new int[4];
+
+      for (int i = 0; i < 4; i++)
+      {
+        Console.Write("Digite um numero:");
+        numeros[i] = int.Parse(Console.ReadLine());
+      }
+
+      Array.Sort(numeros);
+      Console.WriteLine("Os numeros que você digitou em ordem crescente são:");
+
+      for (int i = 0; i < numeros.Length; i++)
+      {
+        Console.WriteLine($"{numeros[i]}");
+      }
+    }
+  }
+}
+
