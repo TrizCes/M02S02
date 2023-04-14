d016722d9e00783ebd9028ec49fff8c87bf54909
Beatriz Ceschini <biaceschini@gmail.com>
2023-04-13 21:52:16 -0300

    O maior número da lista
    Um programa que solicite ao usuário que digite cinco números inteiros e armazene esses números em um array. Em seguida, exiba na tela o maior número digitado.

---
+++ maiorNumero/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    int[] numeros = new int[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+      Console.Write("Digite um numero:");
+      numeros[i] = int.Parse(Console.ReadLine());
+    }
+
+    Array.Sort(numeros);
+    Console.WriteLine($"O maior numero digitado é {numeros[numeros.Length - 1]}");
+  }
+}
