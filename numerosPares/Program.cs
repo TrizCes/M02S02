14716c1035086f9c3ee8f931f07bada87e85771e
Beatriz Ceschini <biaceschini@gmail.com>
2023-04-13 21:35:20 -0300

    Números pares
    Um programa que solicite ao usuário que insira um número inteiro positivo e exiba todos os números pares de 0 até esse número. Se o número digitado pelo usuário for negativo ou zero, exiba uma mensagem de erro e solicite novamente o número.

---
+++ numerosPares/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+  static void Main(string[] args)
+  {
+    int numero = 0;
+
+    while (numero <= 0)
+    {
+      Console.Write("Digite um numero maior que zero: ");
+      numero = int.Parse(Console.ReadLine());
+      if (numero <= 0)
+      {
+        Console.WriteLine("Erro! \n Tente novamente.");
+      }
+    }
+
+    for (int i = 0; i <= numero; i = i + 2)
+    {
+      Console.WriteLine(i);
+    }
+  }
+}
